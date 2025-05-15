04869ca408f8992d31bac204a0dbf1eb1f07ca74
Elkana Maina <elisoft.engineer@gmail.com>
2025-05-15 10:07:55 +0300

    Add user routes

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
-urlpatterns = [
+from . import views
 
+urlpatterns = [
+    path('', views.UserList.as_view(), name="user-list"),
+    path('<uuid:pk>/', views.UserDetail.as_view(), name="user-detail"),
+    path('change-password/', views.ChangePassword.as_view(), name="change-password"),
 ]
