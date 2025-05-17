197ab4ad300e49abde8e2ec0c3487754cac5c065
Elkana Maina <elisoft.engineer@gmail.com>
2025-05-17 12:06:51 +0300

    Add token-related routes

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+
+from . import views
+
 
 urlpatterns = [
+    # authentication
+    path('token/', views.CustomTokenObtainPairView.as_view(), name='token-obtain'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token-verify'),
+    # modules
     path('users/', include('users.urls')),
     path('tasks/', include('tasks.urls')),
 ]
