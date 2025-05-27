86a813ab8875275cdd28a98e392551fcfa2ba224
Elkana Maina <elisoft.engineer@gmail.com>
2025-05-27 11:38:02 +0300

    Add humanized_time property

--- tasks/models.py
+++ tasks/models.py
@@ -1,9 +1,11 @@
+from datetime import timedelta
 import enum
 import uuid
 
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.utils.timezone import now
 from enumfields import EnumField
 
 User = get_user_model()
@@ -31,3 +33,25 @@ class Task(models.Model):
 
     def __str__(self):
         return self.detail
+    
+    @property
+    def humanized_time(self) -> str:
+        diff = now() - self.created_at
+        if diff < timedelta(minutes=1):
+            return "Just now"
+        elif diff < timedelta(hours=1):
+            minutes = int(diff.total_seconds() / 60)
+            return f"{minutes} minute{'s' if minutes > 1 else ''} ago"
+        elif diff < timedelta(days=1):
+            hours = int(diff.total_seconds() / 3600)
+            return f"{hours} hour{'s' if hours > 1 else ''} ago"
+        elif diff < timedelta(days=2):
+            return "Yesterday"
+        elif diff < timedelta(weeks=1):
+            days = diff.days
+            return f"{days} day{'s' if days > 1 else ''} ago"
+        elif diff < timedelta(weeks=4):
+            weeks = diff.days // 7
+            return f"{weeks} week{'s' if weeks > 1 else ''} ago"
+        else:
+            return self.created_at.strftime("%b %d, %Y")
